6efb6f255889b5b161db56d91e8de9eb376866c8
kbatchelor <kbatchelor@insidesales.com>
2016-08-10 17:07:24 -0600

    some more style

--- src/index.js
+++ src/index.js
@@ -28,7 +28,20 @@ class Homepage extends React.Component {
             )
         });
 
-        return <div>{examples}</div>;
+        return (
+            <div className="pageWrapper">
+                <div className="nav">
+                    <div className="navItem"><a>Docs</a></div>
+                    <div className="navItem"><a>Components</a></div>
+                    <div className="navItem"><a>Github</a></div>
+                </div>
+                <div className="header">
+                    Superhero<span className="sub">UI</span>
+                    <div className="subText">react components that are really super awesome</div>
+                </div>
+                <div>{examples}</div>
+            </div>
+        );
     }
 }
 
