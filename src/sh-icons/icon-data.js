b15fdac152108b400c4a6f528bf3b5e6e871d23f
kbatchelor <kbatchelor@insidesales.com>
2016-08-15 16:31:55 -0600

    added icons docs

---
+++ src/sh-icons/icon-data.js
@@ -0,0 +1,314 @@
+export default [
+    {
+        "order": 177,
+        "prevSize": 32,
+        "name": "record"
+    },
+    {
+        "order": 178,
+        "prevSize": 32,
+        "name": "text-bubble"
+    },
+    {
+        "order": 179,
+        "prevSize": 32,
+        "name": "people-three"
+    },
+    {
+        "order": 180,
+        "prevSize": 32,
+        "name": "dial-pad"
+    },
+    {
+        "order": 181,
+        "prevSize": 32,
+        "name": "envelope"
+    },
+    {
+        "order": 182,
+        "prevSize": 32,
+        "name": "end-call"
+    },
+    {
+        "order": 183,
+        "prevSize": 32,
+        "name": "right-arrow"
+    },
+    {
+        "order": 184,
+        "prevSize": 32,
+        "name": "linkedin"
+    },
+    {
+        "order": 185,
+        "prevSize": 32,
+        "name": "neuralytics"
+    },
+    {
+        "order": 186,
+        "prevSize": 32,
+        "name": "phone"
+    },
+    {
+        "order": 187,
+        "prevSize": 32,
+        "name": "clock"
+    },
+    {
+        "order": 188,
+        "prevSize": 32,
+        "codes": [
+            59659,
+            59660,
+            59661,
+            59662,
+            59663
+        ],
+        "name": "phone-volume"
+    },
+    {
+        "order": 189,
+        "prevSize": 32,
+        "name": "calendar"
+    },
+    {
+        "order": 190,
+        "prevSize": 32,
+        "name": "radio-button"
+    },
+    {
+        "order": 191,
+        "prevSize": 32,
+        "name": "radio-button-empty"
+    },
+    {
+        "order": 192,
+        "prevSize": 32,
+        "name": "check-box-empty"
+    },
+    {
+        "order": 193,
+        "prevSize": 32,
+        "name": "text-bubbles"
+    },
+    {
+        "order": 194,
+        "prevSize": 32,
+        "name": "check-box"
+    },
+    {
+        "order": 195,
+        "prevSize": 32,
+        "name": "check"
+    },
+    {
+        "order": 196,
+        "prevSize": 32,
+        "name": "buildings"
+    },
+    {
+        "order": 197,
+        "prevSize": 32,
+        "name": "download"
+    },
+    {
+        "order": 198,
+        "prevSize": 32,
+        "name": "check-outline"
+    },
+    {
+        "order": 199,
+        "prevSize": 32,
+        "name": "bolt"
+    },
+    {
+        "order": 200,
+        "prevSize": 32,
+        "name": "chevron-up"
+    },
+    {
+        "order": 201,
+        "prevSize": 32,
+        "name": "chevron-down"
+    },
+    {
+        "order": 202,
+        "prevSize": 32,
+        "name": "chevron-left"
+    },
+    {
+        "order": 203,
+        "prevSize": 32,
+        "name": "chevron-right"
+    },
+    {
+        "order": 204,
+        "prevSize": 32,
+        "name": "insidesales-logo"
+    },
+    {
+        "order": 205,
+        "prevSize": 32,
+        "name": "paperclip"
+    },
+    {
+        "order": 206,
+        "prevSize": 32,
+        "name": "x"
+    },
+    {
+        "order": 207,
+        "prevSize": 32,
+        "name": "cursor"
+    },
+    {
+        "order": 208,
+        "prevSize": 32,
+        "name": "font"
+    },
+    {
+        "order": 209,
+        "prevSize": 32,
+        "name": "gear"
+    },
+    {
+        "order": 210,
+        "prevSize": 32,
+        "name": "phone-arrow"
+    },
+    {
+        "order": 211,
+        "prevSize": 32,
+        "name": "info"
+    },
+    {
+        "order": 212,
+        "prevSize": 32,
+        "name": "face-neutral"
+    },
+    {
+        "order": 213,
+        "prevSize": 32,
+        "name": "medal"
+    },
+    {
+        "order": 214,
+        "prevSize": 32,
+        "name": "minus"
+    },
+    {
+        "order": 215,
+        "prevSize": 32,
+        "name": "icosahedron"
+    },
+    {
+        "order": 216,
+        "prevSize": 32,
+        "name": "face-sad"
+    },
+    {
+        "order": 217,
+        "prevSize": 32,
+        "name": "plus"
+    },
+    {
+        "order": 218,
+        "prevSize": 32,
+        "name": "search"
+    },
+    {
+        "order": 219,
+        "prevSize": 32,
+        "name": "face-happy"
+    },
+    {
+        "order": 220,
+        "prevSize": 32,
+        "name": "trophy"
+    },
+    {
+        "order": 221,
+        "prevSize": 32,
+        "name": "vision"
+    },
+    {
+        "order": 222,
+        "prevSize": 32,
+        "name": "hourglass"
+    },
+    {
+        "order": 223,
+        "prevSize": 32,
+        "name": "flag"
+    },
+    {
+        "order": 224,
+        "prevSize": 32,
+        "name": "ban"
+    },
+    {
+        "order": 225,
+        "prevSize": 32,
+        "name": "flow-chart"
+    },
+    {
+        "order": 226,
+        "prevSize": 32,
+        "name": "heart"
+    },
+    {
+        "order": 227,
+        "prevSize": 32,
+        "name": "people"
+    },
+    {
+        "order": 228,
+        "prevSize": 32,
+        "name": "glove"
+    },
+    {
+        "order": 229,
+        "prevSize": 32,
+        "name": "twitter"
+    },
+    {
+        "order": 230,
+        "prevSize": 32,
+        "name": "play"
+    },
+    {
+        "order": 231,
+        "prevSize": 32,
+        "name": "expand"
+    },
+    {
+        "order": 232,
+        "prevSize": 32,
+        "name": "collapse"
+    },
+    {
+        "order": 233,
+        "prevSize": 32,
+        "name": "facebook"
+    },
+    {
+        "order": 234,
+        "prevSize": 32,
+        "name": "facebook-negative"
+    },
+    {
+        "order": 235,
+        "prevSize": 32,
+        "name": "laurels"
+    },
+    {
+        "order": 236,
+        "prevSize": 32,
+        "name": "trash"
+    },
+    {
+        "order": 237,
+        "prevSize": 32,
+        "name": "person"
+    }
+];
---
+++ src/sh-icons/usage.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import * as _ from 'lodash';
+import Code from '../util/code';
+import iconList from './icon-data'
+
+require('./example.scss');
+
+let codeText = {};
+
+codeText.cssMain = `
+require('../node_modules/sh-icons/bin/main.css');
+`;
+codeText.cssMain2 = `
+{
+    test: /\\.(ttf|eot|svg|woff)$/,
+    loader: "url-loader"
+},
+`;
+codeText.html = `
+<i className="icon-phone"></i>
+`;
+
+var groupedList = _.chunk(iconList, 4);
+
+class Usage extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            clickMe: 'wide'
+        };
+
+        this.clickMe = this.clickMe.bind(this);
+    }
+
+    clickMe() {
+        this.setState({
+            clickMe: this.state.clickMe === 'wide' ? 'small' : 'wide'
+        });
+    }
+
+    render() {
+        let icons = groupedList.map((iconGroup, index) => {
+            var iconRows = iconGroup.map((icons, iconIndex) => {
+                return (
+                    <td key={iconIndex}>
+                        <i className={"icon-"+icons.name}></i>
+                        <div className="table-label">{icons.name}</div>
+                    </td>
+                )
+            });
+
+            return (
+                <tr key={index}>
+                    {iconRows}
+                </tr>
+            )
+        });
+
+        return (
+            <div>
+                <div className="col code">
+                    <div className="details">Install a new loader to handle the font files.</div>
+                    <pre className="instructions">npm install url-loader --save-dev</pre>
+                    <div className="details">Add the new loader to your list of loaders in webpack.config.js</div>
+                    {Code(codeText.cssMain2, 'javascript')}
+                    <div className="title">HTML</div>
+                    {Code(codeText.html, 'jsx')}
+                </div>
+                <div className="col component">
+                    <div className="title">About</div>
+                    <p>The images are now included as a font. Also CSS classes for each images can now be used to add
+                        images to your page. Simply reference the CSS class in your html.</p>
+                    <div className="title">List of Icons</div>
+                    <div className="details">
+                        <table className="icon-table">
+                            <tbody>
+                                {icons}
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+export default Usage;
