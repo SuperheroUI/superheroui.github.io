a18a869b754471a05c623c9a5860cd80bfb23bcd
kbatchelor <kbatchelor@insidesales.com>
2016-08-22 15:26:21 -0600

    value needs to be a number

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ class Homepage extends React.Component {
                 {name: 'ShTabs', code: <ShTabsExamples />, repo: 'SuperheroUI/shTabs'},
                 {name: 'ShInputSelect', code: <ShInputSelectExamples />, repo: 'SuperheroUI/shInputSelect'},
                 {name: 'ShInputText', code: <ShInputTextExamples />, github: 'https://github.com/SuperheroUI/shInputText'},
-                {name: 'ShInputCurrency', code: <ShInputCurrencyExamples />, github: 'https://github.com/SuperheroUI/shInputCurrency'},
+                {name: 'ShInputCurrency', code: <ShInputCurrencyExamples />, repo: 'SuperheroUI/shInputCurrency'},
                 {name: 'ShInputCheckbox', code: <ShInputCheckboxExamples />, github: 'https://github.com/SuperheroUI/shInputCheckbox'},
                 {name: 'ShIcons', code: <ShIconsExamples />, github: 'https://github.com/SuperheroUI/shIcons'},
                 {name: 'ShButtons', code: <ShButtonsExamples />, github: 'https://github.com/SuperheroUI/shButtons'}
